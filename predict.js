a894325cfba355ee7a8e93b9e8a8a0fbbdb58f24
Larissa Viana <62406351+lrssv@users.noreply.github.com>
2020-07-13 20:14:02 -0300

    Update predict.js

--- predict.js
+++ predict.js
@@ -16,10 +16,10 @@ $( document ).ready(async function () {
 	console.log( "Loading model..." );
 */
 
-/* IMPORTAR O MODELO DE REDES NEURAIS PARA O TENSORFLOW JS*/
+	/*IMPORTA O MODELO DE REDES NEURAIS PARA O TENSORFLOW JS*/
 	model = await tf.loadGraphModel('model/model.json');
 
-/*	
+/*
     console.log( "Model loaded." );
 	$('.progress-bar').hide();
 });
@@ -29,46 +29,42 @@ $("#predict-button").click(async function () {
 	
 	// Pre-process the image
 	console.log( "Loading image..." );
-*/	
+*/
 
-/* CRIAR A VARIAVEL COM A IMAGEM PARA ALIMENTAR A REDE*/ 	
+	/*CRIAR A VARIAVEL COM A IMAGEM PARA ALIMENTAR A REDE*/ 	
 	let tensor = tf.browser.fromPixels(image)
 		.resizeNearestNeighbor([300, 300]) // change the image size
 		.expandDims()
 		.toFloat()
 
-/* VARIAVEL QUE FAZ A PREVISÃO */		
+	/*VARIAVEL QUE FAZ A PREVISÃO */		
 	let predictions = await model.predict(tensor.div(255));
 	
-/* ARMAZENAR DOIS ARRAYS COM O DIAGNÓSTICO E COM A PROBABILIDADE */	
+	/*ARMAZENAR DOIS ARRAYS COM O DIAGNÓSTICO E COM A PROBABILIDADE */	
 	const {values, indices} = tf.topk(predictions,3);
 	
 	/*ARRAY DE PROBABILIDADES*/	
 	const classProbs = values.arraySync();
 
 	/*ARRAY DE DIAGNÓSTICO*/
-    const classIndices = indices.arraySync();
+    	const classIndices = indices.arraySync();
 
-	/*IMPRIME RESULTADO*/
 	console.log(classProbs);
 	console.log(classIndices);
 
-/*
-	let top5 = Array.from(classIndices)
-		.map(function (elem, i) { // this is Array.map
-			return {
-				className: TARGET_CLASSES[i] // we are selecting the value from the obj
-			};
-		}).slice(0,3)
-
-	top5.forEach(function (p){
-		console.log(p)
-	})
-		
+	/*RETORNA O DIAGNÓSTICO EM RELAÇÃO AO VETOR RECEBIDO*/ 
+	let diagnostico = classIndices.map(elem => { const obj = {
+		className: TARGET_CLASSES[elem] 
+	};
+	return obj;
+	});
 	
+	console.log(diagnostico);
+		
+	/*IMPRIME NA TELA OS RESULTADOS*/
 	$("#prediction-list").empty();
 	classProbs.forEach(function (p) {
-		$("#prediction-list").append(`<li>${p.className}: ${p}</li>`);
-		});
+		diagnostico.forEach(function (i){
+			$("#prediction-list").append(`<li>${i.className}: ${p}</li>`);
+		})});
 });
-*/
